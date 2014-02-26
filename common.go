62875824757ed40f424a7ee441ab0f6b4166d80b
David Rocamora <dave@n22t.com>
2014-02-26 17:07:54 -0500

    Retry logic

--- common.go
+++ common.go
@@ -4,10 +4,15 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"math"
 	"net/http"
+	"time"
 
 	"github.com/smartystreets/go-aws-auth"
 )
+
+// MaxTries is the number of times to retry a failing AWS request.
+var MaxTries int = 5
 
 // AWSError is the error document returned from many AWS requests.
 type AWSError struct {
@@ -21,34 +26,45 @@ func (e AWSError) Error() string {
 }
 
 // SendAWSRequest signs and sends an AWS request.
+// It will retry 500s and throttling errors with an exponential backoff.
 func SendAWSRequest(req *http.Request) (*http.Response, error) {
 
 	awsauth.Sign(req)
 	client := &http.Client{}
-	resp, err := client.Do(req)
 
-	if err != nil {
-		return resp, err
-	}
+	for try := 1; try < MaxTries; try++ {
 
-	if resp.StatusCode >= 400 {
-		error := AWSError{}
+		resp, err := client.Do(req)
 		defer resp.Body.Close()
-		errorbuf := new(bytes.Buffer)
-		errorbuf.ReadFrom(resp.Body)
-
-		err = json.Unmarshal(errorbuf.Bytes(), &error)
 		if err != nil {
 			return resp, err
 		}
 
-		if error.Type == "Throttling" {
-			// retry
+		if resp.StatusCode < 400 {
+			// The request succeeded
+			return resp, nil
+		} else {
+			// The request failed, but why?
+			error := AWSError{}
+
+			errorbuf := new(bytes.Buffer)
+			errorbuf.ReadFrom(resp.Body)
+
+			err = json.Unmarshal(errorbuf.Bytes(), &error)
+			if err != nil {
+				return resp, err
+			}
+
+			// If the error wasn't about throttling and it is below 500, lets return it
+			// This retries server errors or AWS errors where we should retry
+			if error.Type != "Throttling" && resp.StatusCode <= 500 {
+				return resp, error
+			}
+
+			// Exponential backoff for the retry
+			sleepDuration := time.Duration(100 * math.Pow(2.0, float64(try)))
+			time.Sleep(sleepDuration * time.Millisecond)
 		}
-
-		return resp, error
-
 	}
-
-	return resp, nil
+	return &http.Response{}, AWSError{Type: "GawsExceededMaxRetries", Message: "The maximum number of retries for this request was exceeded."}
 }
