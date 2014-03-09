adac5bc423773dd7f9e62bf802b7eb2497f08ed1
David Rocamora <dave@n22t.com>
2014-03-09 19:33:58 -0400

    getShardIterator

--- kinesis/kinesis.go
+++ kinesis/kinesis.go
@@ -144,6 +144,7 @@ type Shard struct {
 	ParentShardId         string
 	SequenceNumberRange   SequenceNumberRange
 	ShardId               string
+	stream                *Stream
 }
 
 type StreamDescription struct {
@@ -193,4 +194,47 @@ func (s *Stream) Describe() (StreamDescription, error) {
 		return StreamDescription{}, err
 	}
 	return result.StreamDescription, err
+}
+
+type getShardIteratorResponse struct {
+	ShardIterator string
+}
+
+type getShardIteratorRequest struct {
+	ShardId                string
+	ShardIteratorType      string
+	StartingSequenceNumber string `json:",omitempty"`
+	StreamName             string
+}
+
+// GetShardIterator gets a shard iterator from the shard. It takes a type, which is one of: AT_SEQUENCE_NUMBER, AFTER_SEQUENCE_NUMBER, TRIM_HORIZON, or LATEST and an optional sequence number to start on.
+// See http://docs.aws.amazon.com/kinesis/latest/APIReference/API_GetShardIterator.html for more details.
+func (s *Shard) GetShardIterator(shardIteratorType string, startingSequenceNumber string) (string, error) {
+	url := s.stream.Service.Endpoint
+	result := getShardIteratorResponse{}
+
+	body := getShardIteratorRequest{ShardId: s.ShardId, ShardIteratorType: shardIteratorType, StartingSequenceNumber: startingSequenceNumber, StreamName: s.stream.Name}
+
+	bodyAsJson, err := json.Marshal(body)
+	payload := bytes.NewReader(bodyAsJson)
+
+	req, err := http.NewRequest("POST", url, payload)
+
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("X-Amz-Target", "Kinesis_20131202.DescribeStream")
+	req.Header.Set("Content-Type", "application/x-amz-json-1.1")
+
+	resp, err := gaws.SendAWSRequest(req)
+	if err != nil {
+		return "", err
+	}
+
+	err = json.Unmarshal(resp, &result)
+	if err != nil {
+		return "", err
+	}
+	return result.ShardIterator, err
 }
