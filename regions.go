03a6d6fe2eee247235b2ce9950cf2c38d5b43e77
David Rocamora <dave@n22t.com>
2014-03-19 21:54:07 -0400

    That endpoint thing isn't going to work
    Not sure how to handle it yet, but that was the wrong approach.

--- regions.go
+++ regions.go
@@ -7,25 +7,3 @@ var Region string = "us-east-1"
 type AWSService struct {
 	Endpoint string
 }
-
-var usEast1 = map[string]AWSService{
-	"kinesis":  AWSService{Endpoint: "https://kinesis.us-east-1.amazonaws.com"},
-	"dynamodb": AWSService{Endpoint: "https://dynamodb.us-east-1.amazonaws.com"},
-}
-
-var regionsToServices = map[string]map[string]AWSService{
-	"us-east-1": usEast1,
-}
-
-var noSuchServiceError = gawsError{Type: "GawsNoSuchService", Message: "Could not find this service."}
-
-// ServiceForRegion will return the AWSService for a given region and service name.
-func ServiceForRegion(region string, serviceName string) (AWSService, error) {
-	service := regionsToServices[region][serviceName]
-
-	if service.Endpoint == "" {
-		return service, noSuchServiceError
-	}
-
-	return service, nil
-}
